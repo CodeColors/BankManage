3b1f4e1f7367bf0e3a1306e837efdf326aaa22bb
Hugo CANAL <contact@piafthebest.fr>
2019-09-20 20:25:07 +0200

    Inital Commit(HTML start)

---
+++ admin.php
@@ -0,0 +1,46 @@
+<html>
+  <head>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/css/mdb.min.css" rel="stylesheet">
+  </head>
+  <body style="background-image: url('assets/img/background.png'); ">
+  <header>
+      <nav class="navbar fixed-top navbar-expand-lg navbar-dark blue scrolling-navbar">
+        <a class="navbar-brand" href="#"><strong>Bank of Los Santos</strong></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Stats <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Comptes Clients</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Comptes Banquier</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav nav-flex-icons">
+            <li class="nav-item">
+              <a class="nav-link">Se déconnecter</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  </body>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/js/mdb.min.js"></script>
+</html>
---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+  <head>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/css/mdb.min.css" rel="stylesheet">
+  </head>
+  <body style="background-image: url('assets/img/background.png'); ">
+      <div class="jumbotron text-center blue-grey lighten-5">
+
+        <!-- Title -->
+        <h2 class="card-title h2">Bank of Los Santos</h2>
+
+        <!-- Subtitle -->
+        <p class="indigo-text my-4 font-weight-bold">Welcome on the new website of the Bank of Los Santos</p>
+
+        <!-- Grid row -->
+        <div class="row d-flex justify-content-center">
+
+          <!-- Grid column -->
+          <div class="col-xl-7 pb-2">
+
+            <p class="card-text">Bienvenue sur le site de la banque de Los Santos. Vous pouvez retrouver tous vos prêts et intérêts sur ce site. Vous pouvez de plus prendre des rendez-vous.</p>
+
+          </div>
+          <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+        <hr class="my-4 pb-2">
+
+        <button class="btn blue-gradient btn-rounded">Se connecter <i class="far fa-id-badge ml-1"></i></button>
+        <button class="btn btn-indigo btn-rounded">Prise de rendez-vous <i class="fas fa-envelope-open ml-1"></i></button>
+
+      </div>
+    <!-- Jumbotron -->
+  </body>
+  <!-- JQuery -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/js/mdb.min.js"></script>
+</html>
